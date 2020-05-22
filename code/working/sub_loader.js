9202d557d1b3cceb28ec1eb4b5db3a5685961f16
Denis <denis+github@nesomail.com>
2020-05-22 13:54:58 +0200

    test fix bug

--- code/working/sub_loader.js
+++ code/working/sub_loader.js
@@ -1,8 +1,8 @@
-//load("https://www.alogh.com/Adventure.Land/code/working/main.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/helperFunctions.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/evadeTarget.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/merchantSkills.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/mageSkills.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/priestSkills.js");
-//load("https://www.alogh.com/Adventure.Land/code/working/rangerSkills.js");
-load("https://www.alogh.com/Adventure.Land/code/testing/simpleAssist.js");
+load("https://www.alogh.com/Adventure.Land/code/working/main.js");
+load("https://www.alogh.com/Adventure.Land/code/working/helperFunctions.js");
+load("https://www.alogh.com/Adventure.Land/code/working/evadeTarget.js");
+load("https://www.alogh.com/Adventure.Land/code/working/merchantSkills.js");
+load("https://www.alogh.com/Adventure.Land/code/working/mageSkills.js");
+load("https://www.alogh.com/Adventure.Land/code/working/priestSkills.js");
+load("https://www.alogh.com/Adventure.Land/code/working/rangerSkills.js");
+//load("https://www.alogh.com/Adventure.Land/code/testing/simpleAssist.js");
