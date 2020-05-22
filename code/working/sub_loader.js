68396aa1c315fae768e43342c5c8799caff502c8
Denis <denis+github@nesomail.com>
2020-05-22 14:23:56 +0200

    test fix bug

--- code/working/sub_loader.js
+++ code/working/sub_loader.js
@@ -2,5 +2,5 @@ load("https://www.alogh.com/Adventure.Land/code/working/main.js");
 load("https://www.alogh.com/Adventure.Land/code/working/helperFunctions.js");
 load("https://www.alogh.com/Adventure.Land/code/working/evadeTarget.js");
 load("https://www.alogh.com/Adventure.Land/code/working/merchantSkills.js");
-load("https://www.alogh.com/Adventure.Land/code/working/skills.js");
+load("https://www.alogh.com/Adventure.Land/code/working/combatSkills.js");
 //load("https://www.alogh.com/Adventure.Land/code/testing/simpleAssist.js");
