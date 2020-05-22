83178a4b6c8a644866c8349981716f36864d659b
Denis <denis+github@nesomail.com>
2020-05-22 22:13:40 +0200

    added farmMetrics

--- code/working/sub_loader.js
+++ code/working/sub_loader.js
@@ -3,4 +3,5 @@ load("https://www.alogh.com/Adventure.Land/code/working/helperFunctions.js");
 load("https://www.alogh.com/Adventure.Land/code/working/evadeTarget.js");
 load("https://www.alogh.com/Adventure.Land/code/working/merchantSkills.js");
 load("https://www.alogh.com/Adventure.Land/code/working/combatSkills.js");
+load("https://www.alogh.com/Adventure.Land/code/working/farmMetrics.js");
 //load("https://www.alogh.com/Adventure.Land/code/testing/simpleAssist.js");
